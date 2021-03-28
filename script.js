501dbdbefc5a1190cc85987c4065421e3c9b80b8
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-28 14:23:12 -0400

    Display meal information

--- script.js
+++ script.js
@@ -60,8 +60,36 @@ function getMealById(mealID) {
 
 // Add meal to DOM
 
-function addMealToDOM(meal) {
+function addMealToDOM(meal) { 
+  const ingredients = [];
 
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  singleMealEl.innerHTML = `
+  <div class="single-meal">
+    <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+    <div class="single-meal-info">
+      ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+      ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+    </div>
+    <div class="main">
+      <p>${meal.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+      </ul>
+    </div>
+  </div>
+`;
 }
 
 // Event Listeners
