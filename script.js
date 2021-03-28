69bf13673b1bef4f721c8c556578d95c89cbcbcd
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-28 13:56:25 -0400

    Fetch meal by id

--- script.js
+++ script.js
@@ -47,5 +47,37 @@ function searchMeal(e) {
   }
 }
 
+// Fetch meal by ID
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+  .then(res => res.json())
+  .then(data => {
+    const meal = data.meals[0];
+
+    addMealToDOM(meal);
+  });
+}
+
+// Add meal to DOM
+
+function addMealToDOM(meal) {
+
+}
+
 // Event Listeners
 submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', e => {
+  const mealInfo = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+
+ if (mealInfo) {
+   const mealID = mealInfo.getAttribute('data-mealID');
+   getMealById(mealID);
+ }
+})
